1af3faaa5ea356966adceff4374212c7f7a156ea
alexangel398 <144530872+alexangel398@users.noreply.github.com>
2024-04-05 23:47:38 -0300

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorlController : Controller
+    {
+
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action..."; // Retorna un mensaje predeterminado para la acción Index
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1998)
+        {
+            // Utiliza HtmlEncoder.Default.Encode para proteger la aplicación de entradas malintencionadas
+            // Utiliza cadenas interpoladas para construir el mensaje de saludo
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
